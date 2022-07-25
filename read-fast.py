80d26676ec1e6ec24d6bcb4501c785de595dfd18
ga96wak <max.poeverlein@tum.de>
2022-07-25 20:26:18 +0200

    add longer pause for hyphenated words

--- read-fast.py
+++ read-fast.py
@@ -6,11 +6,16 @@ import time
 import sys
 import argparse
 
+max_word_length = 40
+
+def sleep(wpm):
+    # we add this function to make the code cleaner
+    time.sleep(60./wpm)
+
 parser = argparse.ArgumentParser(description='Read fast.')
 parser.add_argument('infile', metavar='txtfile', type=str, nargs=1, help='the txt file you want to read')
 parser.add_argument('wpm', metavar='wpm', type=float, nargs=1, help='how many words you want to read per minute')
 parser.add_argument('line_begin', metavar='line', type=int, nargs=1, help='at which line of the txt file to start')
-
 args = parser.parse_args()
 
 infile, wpm, line_begin = args.infile[0], args.wpm[0], args.line_begin[0]
@@ -22,17 +27,21 @@ with open(infile, 'r') as f:
 try:
     for line_counter, line in enumerate(lines[line_begin:], start=line_begin):
         line = line.strip().split()
-        for word_counter, word in line:
+        for word_counter, word in enumerate(line):
             print(f'{word:^40} {line_counter:06d} {word_counter:06d}', end='\r')
-            if word.endswith('.') or word.endswith(','):
-                time.sleep(60./wpm)
-    
+
+            sleep(wpm)
+
+            # after the word, we add extra waiting time in certain cases
+            for _ in range(len(word.split('-'))):
+                sleep(wpm)
             if len(word) > 8:
-                time.sleep(60./wpm)
+                sleep(wpm)
+            if word.endswith('.') or word.endswith(','):
+                sleep(wpm)
     
-    
-            time.sleep(60./wpm)
 
 except KeyboardInterrupt:
+    # The current line is displayed when the script is canceled
     print(f'Interrupted at line {line_counter:06d}')
 
