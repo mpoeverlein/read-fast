99e6e73b279eb5786dd8a5b53e484fd46071c4cc
mpoeverlein <maximilian.poeverlein@dbb.su.se>
2022-07-25 20:43:01 +0200

    add option to start at some word within a line

--- read-fast.py
+++ read-fast.py
@@ -13,12 +13,13 @@ def sleep(wpm):
     time.sleep(60./wpm)
 
 parser = argparse.ArgumentParser(description='Read fast.')
-parser.add_argument('infile', metavar='txtfile', type=str, nargs=1, help='the txt file you want to read')
-parser.add_argument('wpm', metavar='wpm', type=float, nargs=1, help='how many words you want to read per minute')
-parser.add_argument('line_begin', metavar='line', type=int, nargs=1, help='at which line of the txt file to start')
+parser.add_argument('infile', metavar='txtfile', type=str, help='the txt file you want to read')
+parser.add_argument('wpm', metavar='wpm', type=float, help='how many words you want to read per minute')
+parser.add_argument('line_begin', metavar='line', type=int, help='at which line of the txt file to start')
+parser.add_argument('word_begin', metavar='word', type=int, nargs='?', help='at which word in the line to start', default=0)
 args = parser.parse_args()
 
-infile, wpm, line_begin = args.infile[0], args.wpm[0], args.line_begin[0]
+infile, wpm, line_begin, word_begin = args.infile, args.wpm, args.line_begin, args.word_begin
 
 # reading that amount of lines is ok for book-sized data
 with open(infile, 'r') as f:
@@ -27,7 +28,7 @@ with open(infile, 'r') as f:
 try:
     for line_counter, line in enumerate(lines[line_begin:], start=line_begin):
         line = line.strip().split()
-        for word_counter, word in enumerate(line):
+        for word_counter, word in enumerate(line[word_begin:], start=word_begin):
             print(f'{word:^40} {line_counter:06d} {word_counter:06d}', end='\r')
 
             sleep(wpm)
@@ -39,6 +40,9 @@ try:
                 sleep(wpm)
             if word.endswith('.') or word.endswith(','):
                 sleep(wpm)
+
+
+        word_begin = 0
     
 
 except KeyboardInterrupt:
